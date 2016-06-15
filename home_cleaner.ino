d16071fb20597e3bda8eb54426431703761d5189
Akash Konda <akash_konda97@hotmail.com>
2016-06-16 00:25:08 +0530

    incomplete

---
+++ home_cleaner.ino
@@ -0,0 +1,345 @@
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+//mpu vars
+#define LED_PIN 13
+bool blinkState = false;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
+
+//my vars
+#define echo1 A0
+#define echo_r A1
+const int enable_r = 3;
+const int trig1 = 4;
+const int enable_l = 5;
+const int trig_r = 6;
+const int echo_l = 7;
+const int trig_l = 8;
+const int motor_r1 = 9;//red
+const int motor_r2 = 10;//black
+const int motor_l1 = 11;//red
+const int motor_l2 = 12;//black
+const int front_distance = 10;
+const int turnradius = 20;
+bool rightturn =false, leftturn = false;
+int runno=1;
+
+//INTERRUPT DETECTION ROUTINE
+
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+float reading(){
+    float reading;
+    mpu.resetFIFO();
+    / / if programming failed, don't try to do anything
+   
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+        // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetEuler(euler, &q);
+            mpu.resetFIFO();
+            return euler[0]*180/M_PI;
+            
+  }
+
+}
+
+long us_reading(int echo_pin, int trig_pin){
+  long duration,distance;
+  digitalWrite(trig_pin,LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trig_pin,HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trig_pin,LOW);
+  duration = pulseIn(echo_pin,HIGH);
+
+  distance = duration/58.2;
+  
+  return distance;
+}
+
+void turn_right(float read_start){
+    float read_final = read_start;
+    if(abs(read_start)<90 || read_start < (-90) ){
+      while(abs(read_final-read_start)<=90){
+       analogWrite(enable_r, 190);
+       analogWrite(enable_l, 190);
+       digitalWrite(motor_r1,HIGH);
+       digitalWrite(motor_r2,LOW);
+       digitalWrite(motor_l1,HIGH);
+       digitalWrite(motor_l2,LOW);
+       //delay(1);//turn right for 1 ms
+       read_final=reading();
+     }  
+    }
+    else if(read_start > 90){
+      float read_final;
+      read_final = read_start;
+      float angle1 = abs(180- read_start);
+      while(true){
+        if(read_final >90){
+          analogWrite(enable_r, 190);
+          analogWrite(enable_l, 190);
+          digitalWrite(motor_r1,HIGH);
+          digitalWrite(motor_r2,LOW);
+          digitalWrite(motor_l1,HIGH);
+          digitalWrite(motor_l2,LOW);
+          
+        }
+        else if((angle1 + abs(180 - abs(read_final)))<90){
+          analogWrite(enable_r, 190);
+          analogWrite(enable_l, 190);
+          digitalWrite(motor_r1,HIGH);
+          digitalWrite(motor_r2,LOW);
+          digitalWrite(motor_l1,HIGH);
+          digitalWrite(motor_l2,LOW);
+          
+        }
+        else break;
+        read_final=reading();
+      }
+    }
+    
+    
+}
+void turn_left(float read_start){
+    /*float read_final = read_start;
+    if(abs(read_start)<90 || read_start >90){
+      while(abs(read_final - read_start)<90){
+       analogWrite(enable_r, 190);
+       analogWrite(enable_l, 190);
+       digitalWrite(motor_r1,LOW);
+       digitalWrite(motor_r2,HIGH);
+       digitalWrite(motor_l1,LOW);
+       digitalWrite(motor_l2,HIGH);
+       //delay(1);//turn left for 1 ms
+       read_final = reading();
+      }
+    }
+    else if(read_start < (-90)){
+      float read_final;
+      read_final = read_start;
+      float angle1 = abs(180- abs(read_start));
+      while(true){
+        if(read_final < -90){
+          analogWrite(enable_r, 190);
+          analogWrite(enable_l, 190);
+          digitalWrite(motor_r1,LOW);
+          digitalWrite(motor_r2,HIGH);
+          digitalWrite(motor_l1,LOW);
+          digitalWrite(motor_l2,HIGH);
+        }
+        else if((angle1 + abs(180- abs(read_final)))<90){
+          analogWrite(enable_r, 190);
+          analogWrite(enable_l, 190);
+          digitalWrite(motor_r1,LOW);
+          digitalWrite(motor_r2,HIGH);
+          digitalWrite(motor_l1,LOW);
+          digitalWrite(motor_l2,HIGH);
+          
+        }
+        else break;
+        read_final = reading();
+      }
+      
+    }*/
+    turn_right(read_start);
+    delay(100);
+    turn_right(read_start);
+    delay(100);
+    turn_right(read_start);
+    delay(100);
+    
+}
+
+
+void setup() {
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    
+    Serial.begin(115200);
+    
+
+    
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
+    /*Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+    */
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(-1125);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(echo1,INPUT);
+  pinMode(echo_r,INPUT);
+  pinMode(echo_l,INPUT);
+  pinMode(trig1,OUTPUT);
+  pinMode(trig_r,OUTPUT);
+  pinMode(trig_l,OUTPUT);
+  pinMode(enable_r,OUTPUT);
+  pinMode(enable_l,OUTPUT);
+  pinMode(motor_r1,OUTPUT);
+  pinMode(motor_r2,OUTPUT);
+  pinMode(motor_l1,OUTPUT);
+  pinMode(motor_l2,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (!dmpReady) return; //dmp not ready
+  if(runno == 1){
+    delay(17000);
+    runno++;
+    
+    
+  }
+
+
+  if(us_reading(echo1, trig1)>=front_distance){
+      //Move Forward
+      digitalWrite(enable_r, HIGH);
+      digitalWrite(enable_l, HIGH);
+      digitalWrite(motor_r1,LOW);
+      digitalWrite(motor_r2,HIGH);
+      digitalWrite(motor_l1,HIGH);
+      digitalWrite(motor_l2,LOW);
+      //delay(50);//move forward for 50ms to avoid excessive load
+    }
+    else{
+      if(us_reading(echo_r,trig_r)>=turnradius && us_reading(echo_l,trig_l)<=turnradius){
+        //Turn Right 
+        //take mpu read and pass to turn
+        turn_right(reading());
+        //Store the current turn value
+        //Actual function of turn right
+          
+        rightturn=true;
+        leftturn=false;      
+      }
+      else if(us_reading(echo_r,trig_r)<=turnradius && us_reading(echo_l,trig_l)>=turnradius){
+        //Turn left
+        //take mpu read and pass to turn
+        turn_left(reading());
+        //Store the current turn value
+           
+        rightturn=false;
+        leftturn=true;     
+      }
+      else if(us_reading(echo_r,trig_r)<=turnradius && us_reading(echo_l,trig_l)<=turnradius){
+        //Stop the bot by making both the enable pins low
+        digitalWrite(enable_r, 0);
+        digitalWrite(enable_l, 0);
+      }
+      else if(us_reading(echo_r,trig_r)>=turnradius && us_reading(echo_l,trig_l)>=turnradius){
+        //Turn according to feedback!!!
+        if(leftturn && !rightturn){
+          //Take right about turn
+          //take mpu read and pass to turn
+          turn_right(reading());
+          
+          
+        }else{
+          //Take left about turn
+          //take mpu read and pass to turn
+          turn_left(reading());
+          
+        }
+      }
+    }
+}
